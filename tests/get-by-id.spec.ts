b8c152f40880e33bd97903d38f8f4afa5316f7c7
tonipra <toni.prabowo@flip.id>
2024-08-01 13:39:37 +0700

    fix typos

--- tests/get-by-id.spec.ts
+++ tests/get-by-id.spec.ts
@@ -19,7 +19,7 @@ test.describe('Get by ID and Check Response Schema', () => {
       expect(responseGetJson.userId).toBe(1);
       expect(responseGetJson.id).toBe(1);
       expect(responseGetJson.title).toBe('sunt aut facere repellat provident occaecati excepturi optio reprehenderit');
-      expect(responseGetJson.title).toBe('quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto');
+      expect(responseGetJson.body).toBe('quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto');
 
       //check json schema
       const valid = ajv.validate(require('./schema/get-by-id.schema.json'), responseGetJson);
--- tests/put.spec.ts
+++ tests/put.spec.ts
@@ -9,7 +9,7 @@ test.describe('Put and Check Response Schema', () => {
       // get by id
       const responsePut = await request.put(`https://jsonplaceholder.typicode.com/posts/1`, {
         data: {
-          id: 101,
+          id: 1,
           title: 'fuuu',
           body: 'bar',
           userId: 1
@@ -23,10 +23,10 @@ test.describe('Put and Check Response Schema', () => {
       const responsePutJson = JSON.parse(await responsePut.text());
 
       // assert value
-      expect(responsePutJson.id).toBe(101);
+      expect(responsePutJson.id).toBe(1);
       expect(responsePutJson.title).toBe('fuuu');
       expect(responsePutJson.body).toBe('bar');
-      expect(responsePutJson.userId).toBe('1');
+      expect(responsePutJson.userId).toBe(1);
 
       //check json schema
       const valid = ajv.validate(require('./schema/get-by-id.schema.json'), responsePutJson);
